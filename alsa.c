b171238423f504ee084b0d0a66c4a4e459da275d
Michael Barbour <barbour.michael.0@gmail.com>
2014-08-04 20:56:48 -0400

    alsa.c: Print warning on xrun.

--- alsa.c
+++ alsa.c
@@ -30,9 +30,11 @@ ssize_t alsa_read(struct codec *c, sample_t *buf, ssize_t frames)
 	try_again:
 	n = snd_pcm_readi(state->dev, (char *) buf, frames);
 	if (n < 0) {
+		if (n == -EPIPE)
+			LOG(LL_ERROR, "dsp: alsa: snd_pcm_readi: warning: overrun occurred\n");
 		n = snd_pcm_recover(state->dev, n, 1);
 		if (n < 0) {
-			LOG(LL_ERROR, "dsp: alsa: snd_pcm_readi: read failed\n");
+			LOG(LL_ERROR, "dsp: alsa: snd_pcm_readi: error: read failed\n");
 			return 0;
 		}
 		else
@@ -53,9 +55,11 @@ ssize_t alsa_write(struct codec *c, sample_t *buf, ssize_t frames)
 		try_again:
 		n = snd_pcm_writei(state->dev, state->buf, n);
 		if (n < 0) {
+			if (n == -EPIPE)
+				LOG(LL_ERROR, "dsp: alsa: snd_pcm_readi: warning: underrun occurred\n");
 			n = snd_pcm_recover(state->dev, n, 1);
 			if (n < 0) {
-				LOG(LL_ERROR, "dsp: alsa: snd_pcm_writei: write failed\n");
+				LOG(LL_ERROR, "dsp: alsa: snd_pcm_writei: error: write failed\n");
 				return i;
 			}
 			else
