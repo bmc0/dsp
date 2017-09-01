4c669f650dfeaae2ff1770f79dcc00a7a2e25295
Michael Barbour <barbour.michael.0@gmail.com>
2017-08-31 22:09:05 -0400

    scripts/rew_to_dsp.sh: Fix {LS,HS} <n>dB parsing.
    Commas were not stripped from the frequency field for {LS,HS} <n>dB
    filters (only a problem for files generated by old versions of REW).

--- scripts/rew_to_dsp.sh
+++ scripts/rew_to_dsp.sh
@@ -12,30 +12,51 @@ AWK_SCRIPT='
 	if ($1=="Preamp:" && $2!=0) {
 		print "gain", $2
 	}
-	else if ($1=="Filter" && $3=="ON" && $9!=0) {
-		gsub(",", "", $6)
-		if ($4=="PK")
+	else if ($1=="Filter" && $3=="ON") {
+		if ($4=="PK" && $9!=0) {
+			gsub(",", "", $6)
 			print "eq", $6, $12, $9
-		else if ($4=="LP")
+		}
+		else if ($4=="LP") {
+			gsub(",", "", $6)
 			print "lowpass", $6, "0.7071"
-		else if ($4=="HP")
+		}
+		else if ($4=="HP") {
+			gsub(",", "", $6)
 			print "highpass", $6, "0.7071"
-		else if ($4=="LPQ")
+		}
+		else if ($4=="LPQ") {
+			gsub(",", "", $6)
 			print "lowpass", $6, $9
-		else if ($4=="HPQ")
+		}
+		else if ($4=="HPQ") {
+			gsub(",", "", $6)
 			print "highpass", $6, $9
-		else if ($4=="NO")
+		}
+		else if ($4=="NO") {
+			gsub(",", "", $6)
 			print "notch", $6, "30.0"
-		else if ($4=="LS" && $5=="Fc")
+		}
+		else if ($4=="LS" && $5=="Fc" && $9!=0) {
+			gsub(",", "", $6)
 			print "lowshelf", $6, "0.9s", $9
-		else if ($4=="HS" && $5=="Fc")
+		}
+		else if ($4=="HS" && $5=="Fc" && $9!=0) {
+			gsub(",", "", $6)
 			print "highshelf", $6, "0.9s", $9
-		else if ($4=="LS" && $5~/[0-9]+dB/)
+		}
+		else if ($4=="LS" && $5~/[0-9]+dB/ && $10!=0) {
+			gsub(",", "", $7)
 			print "lowshelf", $7, substr($5, 1, length($5)-2)"d", $10
-		else if ($4=="HS" && $5~/[0-9]+dB/)
+		}
+		else if ($4=="HS" && $5~/[0-9]+dB/ && $10!=0) {
+			gsub(",", "", $7)
 			print "highshelf", $7, substr($5, 1, length($5)-2)"d", $10
-		else if ($4=="AP")
+		}
+		else if ($4=="AP") {
+			gsub(",", "", $6)
 			print "allpass", $6, $9
+		}
 	}
 }'
 
