bc2427d7eb18d6a7e2bc2a023d76bdc58f5bc297
Michael Barbour <barbour.michael.0@gmail.com>
2024-11-22 15:37:08 -0500

    remix.c: Add fast paths for mixing <= 4 inputs per output.

--- remix.c
+++ remix.c
@@ -23,21 +23,78 @@
 
 #define DEBUG_CHANNEL_MAPPING 0
 
-struct remix_state {
-	char **channel_selectors;
+struct fast_sel_4 {
+	int n, c[4];
 };
 
-sample_t * remix_effect_run(struct effect *e, ssize_t *frames, sample_t *ibuf, sample_t *obuf)
+struct remix_state {
+	char **channel_selectors;
+	union {
+		int *s1;
+		struct fast_sel_4 *s4;
+	} fast_sel;
+};
+
+sample_t * remix_effect_run_generic(struct effect *e, ssize_t *frames, sample_t *ibuf, sample_t *obuf)
 {
 	struct remix_state *state = (struct remix_state *) e->data;
+	sample_t *ibuf_p = ibuf, *obuf_p = obuf;
 	for (ssize_t i = 0; i < *frames; ++i) {
 		for (int k = 0; k < e->ostream.channels; ++k) {
-			obuf[i * e->ostream.channels + k] = 0;
+			obuf_p[k] = 0.0;
 			for (int j = 0; j < e->istream.channels; ++j) {
 				if (GET_BIT(state->channel_selectors[k], j))
-					obuf[i * e->ostream.channels + k] += ibuf[i * e->istream.channels + j];
+					obuf_p[k] += ibuf_p[j];
 			}
 		}
+		ibuf_p += e->istream.channels;
+		obuf_p += e->ostream.channels;
+	}
+	return obuf;
+}
+
+sample_t * remix_effect_run_1a(struct effect *e, ssize_t *frames, sample_t *ibuf, sample_t *obuf)
+{
+	struct remix_state *state = (struct remix_state *) e->data;
+	sample_t *ibuf_p = ibuf, *obuf_p = obuf;
+	for (ssize_t i = 0; i < *frames; ++i) {
+		for (int k = 0; k < e->ostream.channels; ++k)
+			obuf_p[k] = ibuf_p[state->fast_sel.s1[k]];
+		ibuf_p += e->istream.channels;
+		obuf_p += e->ostream.channels;
+	}
+	return obuf;
+}
+
+sample_t * remix_effect_run_4(struct effect *e, ssize_t *frames, sample_t *ibuf, sample_t *obuf)
+{
+	struct remix_state *state = (struct remix_state *) e->data;
+	sample_t *ibuf_p = ibuf, *obuf_p = obuf;
+	for (ssize_t i = 0; i < *frames; ++i) {
+		for (int k = 0; k < e->ostream.channels; ++k) {
+			struct fast_sel_4 *sel = &state->fast_sel.s4[k];
+			obuf_p[k] = 0.0;
+			if (sel->n == 1) {
+				obuf_p[k] += ibuf_p[sel->c[0]];
+			}
+			else if (sel->n == 2) {
+				obuf_p[k] += ibuf_p[sel->c[0]];
+				obuf_p[k] += ibuf_p[sel->c[1]];
+			}
+			else if (sel->n == 3) {
+				obuf_p[k] += ibuf_p[sel->c[0]];
+				obuf_p[k] += ibuf_p[sel->c[1]];
+				obuf_p[k] += ibuf_p[sel->c[2]];
+			}
+			else if (sel->n == 4) {
+				obuf_p[k] += ibuf_p[sel->c[0]];
+				obuf_p[k] += ibuf_p[sel->c[1]];
+				obuf_p[k] += ibuf_p[sel->c[2]];
+				obuf_p[k] += ibuf_p[sel->c[3]];
+			}
+		}
+		ibuf_p += e->istream.channels;
+		obuf_p += e->ostream.channels;
 	}
 	return obuf;
 }
@@ -61,6 +118,7 @@ void remix_effect_destroy(struct effect *e)
 	for (int k = 0; k < e->ostream.channels; ++k)
 		free(state->channel_selectors[k]);
 	free(state->channel_selectors);
+	free(state->fast_sel.s1);
 	free(state);
 }
 
@@ -75,17 +133,21 @@ struct effect * remix_effect_init(const struct effect_info *ei, const struct str
 	}
 
 	const int n_selectors = argc - 1, mask_bits = num_bits_set(channel_selector, istream->channels);
-	int delta = n_selectors - mask_bits;
+	const int delta = n_selectors - mask_bits;
 	const int out_channels = istream->channels + delta;
 
 	state = calloc(1, sizeof(struct remix_state));
 	state->channel_selectors = calloc(out_channels, sizeof(char *));
+	int use_run_1a = 1, use_run_4 = 1;
 	for (int k = 0, i = 0, ch = 0; k < out_channels; ++k, ++ch) {
 		state->channel_selectors[k] = NEW_SELECTOR(istream->channels);
 		if (ch >= istream->channels || GET_BIT(channel_selector, ch)) {
 			if (i < n_selectors) {
 				if (strcmp(argv[i+1], ".") != 0 && parse_selector_masked(argv[i+1], state->channel_selectors[k], channel_selector, istream->channels))
 					goto fail;
+				const int n_sel = num_bits_set(state->channel_selectors[k], istream->channels);
+				if (n_sel != 1) use_run_1a = 0;
+				if (n_sel > 4) use_run_4 = 0;
 				++i;
 			}
 			else {
@@ -108,7 +170,27 @@ struct effect * remix_effect_init(const struct effect_info *ei, const struct str
 	e->istream.channels = istream->channels;
 	e->ostream.channels = out_channels;
 	e->plot_info |= PLOT_INFO_MIX;
-	e->run = remix_effect_run;
+	if (use_run_1a) {
+		state->fast_sel.s1 = calloc(out_channels, sizeof(int));
+		for (int k = 0; k < out_channels; ++k) {
+			int ch = 0;
+			while (ch < istream->channels && !GET_BIT(state->channel_selectors[k], ch)) ++ch;
+			state->fast_sel.s1[k] = ch;
+		}
+		e->run = remix_effect_run_1a;
+	}
+	else if (use_run_4) {
+		state->fast_sel.s4 = calloc(out_channels, sizeof(struct fast_sel_4));
+		for (int k = 0; k < out_channels; ++k) {
+			int n = 0;
+			for (int ch = 0; ch < istream->channels; ++ch)
+				if (GET_BIT(state->channel_selectors[k], ch))
+					state->fast_sel.s4[k].c[n++] = ch;
+			state->fast_sel.s4[k].n = n;
+		}
+		e->run = remix_effect_run_4;
+	}
+	else e->run = remix_effect_run_generic;
 	e->plot = remix_effect_plot;
 	e->destroy = remix_effect_destroy;
 	e->data = state;
@@ -119,6 +201,7 @@ struct effect * remix_effect_init(const struct effect_info *ei, const struct str
 		for (int k = 0; k < out_channels; ++k)
 			free(state->channel_selectors[k]);
 	free(state->channel_selectors);
+	free(state->fast_sel.s1);
 	free(state);
 	return NULL;
 }
