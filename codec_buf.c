bmc0/dsp

commit 42476b17ba17783e6905e3d309397f6896ba9a76

codec_buf: Reorganize some code and simplify usage.

codec_buf.c
@@ -28,25 +28,14 @@
 
 #define CMD_QUEUE_LEN 8
 
-enum write_cmd {
-	WRITE_CMD_DROP_BLOCK_QUEUE,  /* drop blocks in queue (async) */
-	WRITE_CMD_DROP_ALL,          /* as above, but drop output codec buffer as well (async) */
-	WRITE_CMD_PAUSE,             /* pause output codec (async) */
-	WRITE_CMD_UNPAUSE,           /* unpause output codec (async) */
-	WRITE_CMD_DRAIN,             /* if running, write pending blocks and stop (sync) */
-	WRITE_CMD_SYNC,              /* wait for previous command to be read (sync, obviously) */
-	WRITE_CMD_TERM,              /* terminate after stop (async) */
-};
-
-struct buf_block {
-	int frames;
+struct write_block {
 	sample_t *data;
+	void (*error_cb)(int);
+	int frames;
 };
 
 struct write_state {
 	pthread_t thread;
-	struct codec *codec;
-	void (*error_cb)(int);
 	struct {
 		pthread_mutex_t lock;
 		sem_t pending, sync;
@@ -57,7 +46,7 @@ struct write_state {
 			sem_t slots;
 		} cmd;
 		struct {
-			struct buf_block *b;
+			struct write_block *b;
 			char stopped, suspended;
 			int front, back, len, items;
 			int max_block_frames, channels;
@@ -67,8 +56,9 @@ struct write_state {
 	} queue;
 };
 
-static void write_cmd_push(struct write_state *state, enum write_cmd cmd)
+void codec_write_buf_cmd_push(void *state_data, enum codec_write_buf_cmd cmd)
 {
+	struct write_state *state = (struct write_state *) state_data;
 	while (sem_wait(&state->queue.cmd.slots) != 0);
 	pthread_mutex_lock(&state->queue.lock);
 	state->queue.cmd.c[state->queue.cmd.back] = cmd;
@@ -76,20 +66,22 @@ static void write_cmd_push(struct write_state *state, enum write_cmd cmd)
 	++state->queue.cmd.items;
 	pthread_mutex_unlock(&state->queue.lock);
 	sem_post(&state->queue.pending);
-	if (cmd == WRITE_CMD_DRAIN || cmd == WRITE_CMD_SYNC)
+	if (cmd == CODEC_WRITE_BUF_CMD_DRAIN || cmd == CODEC_WRITE_BUF_CMD_SYNC)
 		while (sem_wait(&state->queue.sync) != 0);
 }
 
-static void write_queue_push(struct write_state *state, sample_t *data, ssize_t frames)
+void codec_write_buf_push(void *state_data, sample_t *data, ssize_t frames, void (*error_cb)(int))
 {
+	struct write_state *state = (struct write_state *) state_data;
 	while (frames > 0) {
 		const int block_frames = MINIMUM(state->queue.block.max_block_frames, frames);
 		const int block_samples = block_frames * state->queue.block.channels;
 		while (sem_wait(&state->queue.block.slots) != 0);
 		pthread_mutex_lock(&state->queue.lock);
 		if (!state->queue.error) {
-			struct buf_block *block = &state->queue.block.b[state->queue.block.back];
+			struct write_block *block = &state->queue.block.b[state->queue.block.back];
 			block->frames = block_frames;
+			block->error_cb = error_cb;
 			memcpy(block->data, data, block_samples * sizeof(sample_t));
 			state->queue.block.back = (state->queue.block.back+1 < state->queue.block.len) ? state->queue.block.back+1 : 0;
 			state->queue.block.fill_frames += block_frames;
@@ -115,7 +107,7 @@ static void write_queue_drop(struct write_state *state)
 		if (!state->queue.block.suspended)
 			while (sem_trywait(&state->queue.pending) < 0 && errno == EINTR);
 		state->queue.block.back = (state->queue.block.back > 0) ? state->queue.block.back-1 : state->queue.block.len-1;
-		struct buf_block *block = &state->queue.block.b[state->queue.block.back];
+		struct write_block *block = &state->queue.block.b[state->queue.block.back];
 		state->queue.block.fill_frames -= block->frames;
 		--state->queue.block.items;
 		sem_post(&state->queue.block.slots);
@@ -143,40 +135,42 @@ static void write_queue_restore(struct write_state *state)
 
 static void * write_worker(void *arg)
 {
-	struct write_state *state = (struct write_state *) arg;
+	struct codec_write_buf *wb = (struct codec_write_buf *) arg;
+	struct codec *codec = wb->codec;
+	struct write_state *state = (struct write_state *) wb->data;
 	char done = 0, drain = 0;
 	while (!(done && state->queue.block.stopped)) {
 		while (sem_wait(&state->queue.pending) != 0);
 		pthread_mutex_lock(&state->queue.lock);
 		if (state->queue.cmd.items > 0) {
-			enum write_cmd cmd = state->queue.cmd.c[state->queue.cmd.front];
+			enum codec_write_buf_cmd cmd = state->queue.cmd.c[state->queue.cmd.front];
 			state->queue.cmd.front = (state->queue.cmd.front+1) % CMD_QUEUE_LEN;
 			--state->queue.cmd.items;
 			switch (cmd) {
-			case WRITE_CMD_DROP_ALL:
-				if (!state->queue.error) state->codec->drop(state->codec);
-			case WRITE_CMD_DROP_BLOCK_QUEUE:
+			case CODEC_WRITE_BUF_CMD_DROP_ALL:
+				if (!state->queue.error) codec->drop(codec);
+			case CODEC_WRITE_BUF_CMD_DROP_BLOCK_QUEUE:
 				write_queue_drop(state);
 				break;
-			case WRITE_CMD_PAUSE:
-				if (!state->queue.error) state->codec->pause(state->codec, 1);
+			case CODEC_WRITE_BUF_CMD_PAUSE:
+				if (!state->queue.error) codec->pause(codec, 1);
 				write_queue_suspend(state);
 				break;
-			case WRITE_CMD_UNPAUSE:
-				if (!state->queue.error) state->codec->pause(state->codec, 0);
+			case CODEC_WRITE_BUF_CMD_UNPAUSE:
+				if (!state->queue.error) codec->pause(codec, 0);
 				write_queue_restore(state);
 				break;
-			case WRITE_CMD_DRAIN:
+			case CODEC_WRITE_BUF_CMD_DRAIN:
 				if (state->queue.block.suspended)
 					write_queue_drop(state);
 				if (state->queue.block.stopped)
 					sem_post(&state->queue.sync);
 				else drain = 1;
 				break;
-			case WRITE_CMD_SYNC:
+			case CODEC_WRITE_BUF_CMD_SYNC:
 				sem_post(&state->queue.sync);
 				break;
-			case WRITE_CMD_TERM:
+			case CODEC_WRITE_BUF_CMD_TERM:
 				done = 1;
 				break;
 			default:
@@ -186,7 +180,7 @@ static void * write_worker(void *arg)
 			sem_post(&state->queue.cmd.slots);
 		}
 		else if (!state->queue.block.suspended && state->queue.block.items > 0) {
-			struct buf_block *block = &state->queue.block.b[state->queue.block.front];
+			struct write_block *block = &state->queue.block.b[state->queue.block.front];
 			state->queue.block.front = (state->queue.block.front+1 < state->queue.block.len) ? state->queue.block.front+1 : 0;
 			state->queue.block.fill_frames -= block->frames;
 			--state->queue.block.items;
@@ -194,13 +188,13 @@ static void * write_worker(void *arg)
 			pthread_mutex_unlock(&state->queue.lock);
 
 			if (!state->queue.error && block->frames > 0) {
-				if (state->codec->write(state->codec, block->data, block->frames) != block->frames) {
+				if (codec->write(codec, block->data, block->frames) != block->frames) {
 					pthread_mutex_lock(&state->queue.lock);
 					state->queue.error = 1;
 					write_queue_drop(state);
 					pthread_mutex_unlock(&state->queue.lock);
-					if (state->error_cb)
-						state->error_cb(CODEC_BUF_ERROR_SHORT_WRITE);
+					if (block->error_cb)
+						block->error_cb(CODEC_BUF_ERROR_SHORT_WRITE);
 				}
 			}
 			sem_post(&state->queue.block.slots);
@@ -217,46 +211,16 @@ static void * write_worker(void *arg)
 	return NULL;
 }
 
-void codec_write_buf_write(struct codec_write_buf *wb, sample_t *buf, ssize_t frames)
-{
-	struct write_state *state = (struct write_state *) wb->data;
-	write_queue_push(state, buf, frames);
-}
-
-ssize_t codec_write_buf_delay(struct codec_write_buf *wb)
+ssize_t codec_write_buf_delay_nw(struct codec_write_buf *wb)
 {
 	struct write_state *state = (struct write_state *) wb->data;
 	pthread_mutex_lock(&state->queue.lock);
-	ssize_t d = state->queue.block.fill_frames + state->codec->delay(state->codec);
+	ssize_t d = state->queue.block.fill_frames + wb->codec->delay(wb->codec);
 	pthread_mutex_unlock(&state->queue.lock);
 	return d;
 }
 
-void codec_write_buf_drop(struct codec_write_buf *wb, int drop_all)
-{
-	struct write_state *state = (struct write_state *) wb->data;
-	write_cmd_push(state, (drop_all) ? WRITE_CMD_DROP_ALL : WRITE_CMD_DROP_BLOCK_QUEUE);
-}
-
-void codec_write_buf_pause(struct codec_write_buf *wb, int pause)
-{
-	struct write_state *state = (struct write_state *) wb->data;
-	write_cmd_push(state, (pause) ? WRITE_CMD_PAUSE : WRITE_CMD_UNPAUSE);
-}
-
-void codec_write_buf_drain(struct codec_write_buf *wb)
-{
-	struct write_state *state = (struct write_state *) wb->data;
-	write_cmd_push(state, WRITE_CMD_DRAIN);
-}
-
-void codec_write_buf_sync(struct codec_write_buf *wb)
-{
-	struct write_state *state = (struct write_state *) wb->data;
-	write_cmd_push(state, WRITE_CMD_SYNC);
-}
-
-void write_state_destroy(struct write_state *state)
+static void write_state_destroy(struct write_state *state)
 {
 	pthread_mutex_destroy(&state->queue.lock);
 	sem_destroy(&state->queue.pending);
@@ -269,51 +233,46 @@ void write_state_destroy(struct write_state *state)
 	free(state);
 }
 
-void codec_write_buf_destroy(struct codec_write_buf *wb)
+void codec_write_buf_destroy_nw(struct codec_write_buf *wb)
 {
 	struct write_state *state = (struct write_state *) wb->data;
-	write_cmd_push(state, WRITE_CMD_DRAIN);
-	write_cmd_push(state, WRITE_CMD_TERM);
+	codec_write_buf_cmd_push(state, CODEC_WRITE_BUF_CMD_DRAIN);
+	codec_write_buf_cmd_push(state, CODEC_WRITE_BUF_CMD_TERM);
 	pthread_join(state->thread, NULL);
 	write_state_destroy(state);
-	free(wb);
 }
 
-struct codec_write_buf * codec_write_buf_init(struct codec *c, int block_frames, int n_blocks, void (*error_cb)(int))
+struct codec_write_buf * codec_write_buf_init(struct codec *codec, int block_frames, int n_blocks)
 {
-	struct write_state *state = calloc(1, sizeof(struct write_state));
-	state->codec = c;
+	struct codec_write_buf *wb = calloc(1, sizeof(struct codec_write_buf));
+	wb->codec = codec;
 
+	if (n_blocks < 2 || (codec->hints & CODEC_HINT_NO_OUT_BUF))
+		return wb;
+
+	struct write_state *state = calloc(1, sizeof(struct write_state));
 	pthread_mutex_init(&state->queue.lock, NULL);
 	sem_init(&state->queue.pending, 0, 0);
 	sem_init(&state->queue.sync, 0, 0);
-	state->error_cb = error_cb;
 	sem_init(&state->queue.cmd.slots, 0, CMD_QUEUE_LEN);
 	state->queue.block.stopped = 1;
 	state->queue.block.len = MAXIMUM(n_blocks, 2);
-	state->queue.block.channels = c->channels;
+	state->queue.block.channels = codec->channels;
 	state->queue.block.max_block_frames = MAXIMUM(block_frames, 8);
-	state->queue.block.b = calloc(n_blocks, sizeof(struct buf_block));
-	state->queue.block.b[0].data = calloc(block_frames * n_blocks * c->channels, sizeof(sample_t));
+	state->queue.block.b = calloc(n_blocks, sizeof(struct write_block));
+	state->queue.block.b[0].data = calloc(block_frames * n_blocks * codec->channels, sizeof(sample_t));
 	for (int i = 1; i < n_blocks; ++i)
-		state->queue.block.b[i].data = state->queue.block.b[0].data + (block_frames * c->channels * i);
+		state->queue.block.b[i].data = state->queue.block.b[0].data + (block_frames * codec->channels * i);
 	sem_init(&state->queue.block.slots, 0, n_blocks);
 
-	if ((errno = pthread_create(&state->thread, NULL, write_worker, state)) != 0) {
+	if ((errno = pthread_create(&state->thread, NULL, write_worker, wb)) != 0) {
 		LOG_FMT(LL_ERROR, "%s(): error: pthread_create() failed: %s", __func__, strerror(errno));
 		write_state_destroy(state);
-		return NULL;
+		return wb;
 	}
 
-	struct codec_write_buf *wb = calloc(1, sizeof(struct codec_write_buf));
-	wb->write = codec_write_buf_write;
-	wb->delay = codec_write_buf_delay;
-	wb->drop = codec_write_buf_drop;
-	wb->pause = codec_write_buf_pause;
-	wb->drain = codec_write_buf_drain;
-	wb->sync = codec_write_buf_sync;
-	wb->destroy = codec_write_buf_destroy;
 	wb->data = state;
+	LOG_S(LL_VERBOSE, "info: write buffer enabled");
 
 	return wb;
 }

dsp.c
@@ -241,8 +241,8 @@ static void cleanup_and_exit(int s)
 		sem_destroy(&ev_queue.items);
 	}
 	destroy_codec_list(&in_codecs);
-	if (out_codec_buf) out_codec_buf->destroy(out_codec_buf);
-	if (out_codec) destroy_codec(out_codec);
+	codec_write_buf_destroy(out_codec_buf);
+	destroy_codec(out_codec);
 	destroy_effects_chain(&chain);
 	free(buf1);
 	free(buf2);
@@ -428,10 +428,7 @@ static void print_io_info(struct codec *c, int ll, const char *n)
 static void get_delay_sec(double *chain_delay, double *out_delay)
 {
 	*chain_delay = get_effects_chain_delay(&chain);
-	if (out_codec_buf)
-		*out_delay = (double) out_codec_buf->delay(out_codec_buf) / out_codec->fs;
-	else
-		*out_delay = (double) out_codec->delay(out_codec) / out_codec->fs;
+	*out_delay = (double) codec_write_buf_delay(out_codec_buf) / out_codec->fs;
 }
 
 static ssize_t get_delay_frames(double fs, double chain_delay_s, double out_delay_s)
@@ -518,12 +515,7 @@ static void write_out(ssize_t frames, sample_t *buf, int do_dither)
 		for (ssize_t i = 0; i < samples; ++i)
 			buf[i] = clip(buf[i]);
 	}
-	if (out_codec_buf)
-		out_codec_buf->write(out_codec_buf, buf, frames);
-	else {
-		if (out_codec->write(out_codec, buf, frames) != frames)
-			write_buf_error_cb(CODEC_BUF_ERROR_SHORT_WRITE);
-	}
+	codec_write_buf_write(out_codec_buf, buf, frames, write_buf_error_cb);
 }
 
 static ssize_t do_seek(struct codec *in, ssize_t pos, ssize_t offset, int whence, int pause_state)
@@ -541,11 +533,7 @@ static ssize_t do_seek(struct codec *in, ssize_t pos, ssize_t offset, int whence
 	}
 	if ((s = in->seek(in, s)) >= 0) {
 		reset_effects_chain(&chain);
-		if (out_codec_buf) {
-			out_codec_buf->drop(out_codec_buf, pause_state);
-			if (pause_state) out_codec_buf->sync(out_codec_buf);
-		}
-		else out_codec->drop(out_codec);
+		codec_write_buf_drop(out_codec_buf, pause_state, pause_state);
 		return s;
 	}
 	return pos;
@@ -554,11 +542,7 @@ static ssize_t do_seek(struct codec *in, ssize_t pos, ssize_t offset, int whence
 static void do_pause(struct codec *in, int pause_state, int sync)
 {
 	if (in) in->pause(in, pause_state);
-	if (out_codec_buf) {
-		out_codec_buf->pause(out_codec_buf, pause_state);
-		if (sync) out_codec_buf->sync(out_codec_buf);
-	}
-	else out_codec->pause(out_codec, pause_state);
+	codec_write_buf_pause(out_codec_buf, pause_state, sync);
 }
 
 static struct codec * init_out_codec(struct codec_params *out_p, struct stream_info *stream, ssize_t frames, int write_buf_blocks)
@@ -584,11 +568,7 @@ static struct codec * init_out_codec(struct codec_params *out_p, struct stream_i
 	out_codec->frames = frames;
 	print_io_info(out_codec, LL_NORMAL, "output");
 
-	if (write_buf_blocks > 1 && !(out_codec->hints & CODEC_HINT_NO_OUT_BUF))
-		out_codec_buf = codec_write_buf_init(out_codec, p.block_frames, write_buf_blocks, write_buf_error_cb);
-	if (out_codec_buf)
-		LOG_S(LL_VERBOSE, "info: write buffer enabled");
-
+	out_codec_buf = codec_write_buf_init(out_codec, p.block_frames, write_buf_blocks);
 	return out_codec;
 }
 
@@ -633,7 +613,7 @@ static void handle_tstp(int is_paused)
 	do { \
 		if (out_codec->fs != stream.fs || out_codec->channels != stream.channels) { \
 			LOG_S(LL_NORMAL, "info: output sample rate and/or channels changed; reopening output"); \
-			if (out_codec_buf) out_codec_buf->destroy(out_codec_buf); \
+			codec_write_buf_destroy(out_codec_buf); \
 			out_codec_buf = NULL; \
 			destroy_codec(out_codec); \
 			if (init_out_codec(&out_p, &stream, -1, write_buf_blocks) == NULL) \
@@ -802,8 +782,7 @@ int main(int argc, char *argv[])
 							pos = do_seek(in_codecs.head, pos, 0, SEEK_SET, is_paused);
 							break;
 						case 'n':
-							if (out_codec_buf) out_codec_buf->drop(out_codec_buf, is_paused);
-							else out_codec->drop(out_codec);
+							codec_write_buf_drop(out_codec_buf, is_paused, 0);
 							reset_effects_chain(&chain);
 							goto next_input;
 						case 'c':
@@ -838,8 +817,7 @@ int main(int argc, char *argv[])
 							signal_effects_chain(&chain);
 							break;
 						case 'q':
-							if (out_codec_buf) out_codec_buf->drop(out_codec_buf, 1);
-							else out_codec->drop(out_codec);
+							codec_write_buf_drop(out_codec_buf, 1, 0);
 							goto end_rw_loop;
 						}
 						break;
@@ -883,7 +861,6 @@ int main(int argc, char *argv[])
 	got_term_sig:
 	clear_progress(1);
 	LOG_FMT(LL_NORMAL, "info: signal %d: terminating...", term_sig);
-	if (out_codec_buf) out_codec_buf->drop(out_codec_buf, 1);
-	else if (out_codec) out_codec->drop(out_codec);
+	codec_write_buf_drop(out_codec_buf, 1, 0);
 	goto end_rw_loop;
 }

codec_buf.h
@@ -22,21 +22,74 @@
 #include "dsp.h"
 #include "codec.h"
 
+/* Internal use only */
+
+enum codec_write_buf_cmd {
+	CODEC_WRITE_BUF_CMD_SYNC = 1,          /* wait for previous command to be read (sync, obviously) */
+	CODEC_WRITE_BUF_CMD_DROP_BLOCK_QUEUE,  /* drop blocks in queue (async) */
+	CODEC_WRITE_BUF_CMD_DROP_ALL,          /* as above, but drop output codec buffer as well (async) */
+	CODEC_WRITE_BUF_CMD_PAUSE,             /* pause output codec (async) */
+	CODEC_WRITE_BUF_CMD_UNPAUSE,           /* unpause output codec (async) */
+	CODEC_WRITE_BUF_CMD_DRAIN,             /* if running, write pending blocks and stop (sync) */
+	CODEC_WRITE_BUF_CMD_TERM,              /* terminate after stop (async) */
+};
+
+struct codec_write_buf {
+	struct codec *codec;
+	void *data;
+};
+
+void codec_write_buf_cmd_push(void *, enum codec_write_buf_cmd);
+void codec_write_buf_push(void *, sample_t *, ssize_t, void (*)(int));
+ssize_t codec_write_buf_delay_nw(struct codec_write_buf *);
+void codec_write_buf_destroy_nw(struct codec_write_buf *);
+
+/* Public API */
+
 enum {
 	CODEC_BUF_ERROR_SHORT_WRITE = 1,
 };
 
-struct codec_write_buf {
-	void (*write)(struct codec_write_buf *, sample_t *, ssize_t);
-	ssize_t (*delay)(struct codec_write_buf *);
-	void (*drop)(struct codec_write_buf *, int);
-	void (*pause)(struct codec_write_buf *, int);
-	void (*drain)(struct codec_write_buf *);
-	void (*sync)(struct codec_write_buf *);
-	void (*destroy)(struct codec_write_buf *);
-	void *data;
-};
+struct codec_write_buf * codec_write_buf_init(struct codec *, int, int);
 
-struct codec_write_buf * codec_write_buf_init(struct codec *, int, int, void (*)(int));
+static inline void codec_write_buf_write(struct codec_write_buf *wb, sample_t *data, ssize_t frames, void (*error_cb)(int))
+{
+	if (wb->data) codec_write_buf_push(wb->data, data, frames, error_cb);
+	else {
+		if (wb->codec->write(wb->codec, data, frames) != frames)
+			error_cb(CODEC_BUF_ERROR_SHORT_WRITE);
+	}
+}
+
+static inline ssize_t codec_write_buf_delay(struct codec_write_buf *wb)
+{
+	if (wb->data) return codec_write_buf_delay_nw(wb);
+	return wb->codec->delay(wb->codec);
+}
+
+static inline void codec_write_buf_drop(struct codec_write_buf *wb, int drop_all, int sync)
+{
+	if (wb->data) {
+		codec_write_buf_cmd_push(wb->data, (drop_all) ? CODEC_WRITE_BUF_CMD_DROP_ALL : CODEC_WRITE_BUF_CMD_DROP_BLOCK_QUEUE);
+		if (sync) codec_write_buf_cmd_push(wb->data, CODEC_WRITE_BUF_CMD_SYNC);
+	}
+	else wb->codec->drop(wb->codec);
+}
+
+static inline void codec_write_buf_pause(struct codec_write_buf *wb, int pause_state, int sync)
+{
+	if (wb->data) {
+		codec_write_buf_cmd_push(wb->data, (pause_state) ? CODEC_WRITE_BUF_CMD_PAUSE : CODEC_WRITE_BUF_CMD_UNPAUSE);
+		if (sync) codec_write_buf_cmd_push(wb->data, CODEC_WRITE_BUF_CMD_SYNC);
+	}
+	else wb->codec->pause(wb->codec, pause_state);
+}
+
+static inline void codec_write_buf_destroy(struct codec_write_buf *wb)
+{
+	if (wb == NULL) return;
+	if (wb->data) codec_write_buf_destroy_nw(wb);
+	free(wb);
+}
 
 #endif

commit 3b02af71a27a80b11169a939fac3625193fb5961

codec_buf.c: Fix race condition introduced in 42476b1.
Also abort if spawning worker thread fails instead of going ahead
with no buffer.

codec_buf.c
@@ -264,15 +264,15 @@ struct codec_write_buf * codec_write_buf_init(struct codec *codec, int block_fra
 	for (int i = 1; i < n_blocks; ++i)
 		state->queue.block.b[i].data = state->queue.block.b[0].data + (block_samples * i);
 	sem_init(&state->queue.block.slots, 0, n_blocks);
+	wb->data = state;
 
 	if ((errno = pthread_create(&state->thread, NULL, write_worker, wb)) != 0) {
 		LOG_FMT(LL_ERROR, "%s(): error: pthread_create() failed: %s", __func__, strerror(errno));
 		write_state_destroy(state);
-		return wb;
+		free(wb);
+		return NULL;
 	}
 
-	wb->data = state;
 	LOG_S(LL_VERBOSE, "info: write buffer enabled");
-
 	return wb;
 }

dsp.c
@@ -545,7 +545,7 @@ static void do_pause(struct codec *in, int pause_state, int sync)
 	codec_write_buf_pause(out_codec_buf, pause_state, sync);
 }
 
-static struct codec * init_out_codec(struct codec_params *out_p, struct stream_info *stream, ssize_t frames, int write_buf_blocks)
+static struct codec_write_buf * init_out_codec(struct codec_params *out_p, struct stream_info *stream, ssize_t frames, int write_buf_blocks)
 {
 	struct codec_params p = *out_p;
 	if (p.path == NULL)  p.path = CODEC_DEFAULT_DEVICE;
@@ -573,7 +573,7 @@ static struct codec * init_out_codec(struct codec_params *out_p, struct stream_i
 	print_io_info(out_codec, LL_NORMAL, "output");
 
 	out_codec_buf = codec_write_buf_init(out_codec, p.block_frames, write_buf_blocks - out_codec->buf_ratio, write_buf_error_cb);
-	return out_codec;
+	return out_codec_buf;
 }
 
 static void handle_tstp(int is_paused)